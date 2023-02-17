ec9368cb9d4292fa4f19c5336cb0c70e7fb11a8b
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-02-16 22:18:04 -0500

    Initial use of openAPI

--- Program.cs
+++ Program.cs
@@ -2,11 +2,19 @@
 
 // https://www.nuget.org/packages/YoutubeExplode
 
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
 using YoutubeExplode;
 
 string youtubeUrl = "https://www.youtube.com/watch?v=LgWRbXw9dRU";
 
 var youtube = new YoutubeClient();
+
+var openAiService = new OpenAIService(new OpenAI.GPT3.OpenAiOptions()
+{
+    ApiKey = ""
+});
 
 // You can specify either video ID or URL
 var video = await youtube.Videos.GetAsync(youtubeUrl);
@@ -42,12 +50,45 @@ for (int l = 0; l < slices; l++)
     var caption = track.Captions[startIndex];
     await file.WriteLineAsync($"TIMESTAMP: {caption.Offset}");
     Console.WriteLine($"Starting: {startIndex} Ending: {endIndex}");
+    string words = "";
 
     for (int k = startIndex; k < endIndex; k++)
     {
+
         caption = track.Captions[k];
-        await file.WriteLineAsync($"{caption.Text}");
+        if (!string.IsNullOrWhiteSpace(caption.Text))
+        {
+            words += $"{caption.Text} ";
+            //await file.WriteLineAsync($"{caption.Text}");
+        }
+        // TODO: Make Open API calls for every time stamp chunk and save completion to a file. 
     }
+    var completionResult = await openAiService.Completions.CreateCompletion(new CompletionCreateRequest()
+    {
+        Prompt = $"(Summarize the following in 10 words: {words} Summary:",
+        Model = Models.TextDavinciV3,
+        Temperature = (float?)0.67,
+        TopP = 1,
+        MaxTokens = 256,
+        FrequencyPenalty = 0,
+        PresencePenalty = 0
+
+    });
+
+    if (completionResult.Successful)
+    {
+        Console.WriteLine(completionResult.Choices.FirstOrDefault());
+        await file.WriteLineAsync($"{completionResult.Choices.FirstOrDefault()}");
+    }
+    else
+    {
+        if (completionResult.Error == null)
+        {
+            throw new Exception("Unknown Error");
+        }
+        Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+    }
+
     if (endIndex + captionsPerSlice < track.Captions.Count)
     {
         startIndex = startIndex + captionsPerSlice;
@@ -56,15 +97,18 @@ for (int l = 0; l < slices; l++)
 }
 
 Console.WriteLine($"Last Starting: {startIndex} Ending: {endIndex}");
-
+/*
 for (var m = endIndex; m < track.Captions.Count; m++)
 {
     var caption = track.Captions[m];
-    await file.WriteLineAsync($"{caption.Text}");
-}
+    // Check if the last caption is not empty
+    if (!string.IsNullOrWhiteSpace(caption.Text))
+    {
+        await file.WriteLineAsync($"{caption.Text}");
+    }
 
-/*for (int i = 0; i < track.Captions.Count; i++)
-{
-    var caption = track.Captions[i];
-    await file.WriteLineAsync($"{caption.Text}");
 }*/
+
+// TODO: Make Open API calls for every time stamp chunk and save completion to a file. 
+
+
