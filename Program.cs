c361de5360f0d916065e4916209db9b663b27b1a
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-02-16 20:05:50 -0500

    implemented slicing method

--- Program.cs
+++ Program.cs
@@ -11,17 +11,17 @@ var youtube = new YoutubeClient();
 // You can specify either video ID or URL
 var video = await youtube.Videos.GetAsync(youtubeUrl);
 
-var title = video.Title; // "Collections - Blender 2.80 Fundamentals"
-var author = video.Author.ChannelTitle; // "Blender"
+//var title = video.Title; // "Collections - Blender 2.80 Fundamentals"
+//var author = video.Author.ChannelTitle; // "Blender"
 var duration = video.Duration; // 00:07:20
-var slices = duration.Value.TotalMinutes / 5;
-
-Console.WriteLine($"The amount of slices is {slices}");
-
-using StreamWriter file = new("WriteLines2.txt", append: true);
 
 
-Console.WriteLine($"The tile is: {title}, it was created by {author} and it is {duration} long");
+//Console.WriteLine($"The amount of slices is {slices}");
+
+using StreamWriter file = new("WriteLines.txt", append: true);
+
+
+//Console.WriteLine($"The tile is: {title}, it was created by {author} and it is {duration} long");
 
 var trackManifest = await youtube.Videos.ClosedCaptions.GetManifestAsync(
     youtubeUrl
@@ -32,34 +32,79 @@ var trackInfo = trackManifest.GetByLanguage("en");
 
 var track = await youtube.Videos.ClosedCaptions.GetAsync(trackInfo);
 
- int interval = Convert.ToInt32(Math.Floor(slices));
- int index = 0;
+Console.WriteLine($"The amount of captions is {track.Captions.Count}");
 
-for (int j = index; j < interval; j++)
+var slices = duration.Value.TotalMinutes / 5; // 11.6444
+
+int sliceCount = Convert.ToInt32(Math.Floor(slices)); // 11 left over .6444
+int startIndex = 0;
+
+double captionIndex = track.Captions.Count / sliceCount;
+
+int captionSlices = Convert.ToInt32(Math.Floor(captionIndex));
+
+Console.WriteLine($"The amount of captionSlices per slice is {captionSlices}");
+Console.WriteLine($"The amount of slices is {sliceCount}");
+
+//int endIndex = captionSlices;
+
+/*for (int i = 0; i < sliceCount; i++)
 {
-  
-    
-    var caption = track.Captions[j];
-
+    var caption = track.Captions[startIndex];
     await file.WriteLineAsync($"TIMESTAMP: {caption.Offset}");
     
-
-    for ( var i = index; i < (track.Captions.Count/interval); i++)
+    for ( var j = startIndex; j <= endIndex; j++)
     {
-        //var caption = track.Captions[i];
-        //var newcaption = caption.Captions[i];
-        caption = track.Captions[i];
+        caption = track.Captions[j];
         await file.WriteLineAsync($"{caption.Text}");
         //Console.WriteLine($"From: {caption.Text}");
     }
 
-    index += interval;
-}
+    startIndex = startIndex+(captionSlices/5);
+    endIndex = endIndex+(captionSlices/5);
+}*/
 // Get the caption displayed at 0:35
 //var caption = track.GetByTime(TimeSpan.FromMinutes(15));
 //var text = caption.Text; // "collection acts as the parent collection"
 
 //Console.WriteLine(text);
+
+int index = 0;
+int endIndex = track.Captions.Count / sliceCount;
+
+for (int l = 0; l < sliceCount; l++)
+{
+    var caption = track.Captions[l];
+    await file.WriteLineAsync($"TIMESTAMP: {caption.Offset}");
+    Console.WriteLine($"Starting: {index} Ending: {endIndex}");
+
+    for (int k = index; k < endIndex; k++)
+    {
+
+        caption = track.Captions[k];
+        //await file.WriteLineAsync($"TIMESTAMP: {caption.Offset}");
+        await file.WriteLineAsync($"{caption.Text}");
+    }
+
+if (endIndex+captionSlices < track.Captions.Count){
+index = index + captionSlices;
+        endIndex = endIndex + captionSlices;
+}
+    
+        
+    
+    
+}
+
+Console.WriteLine($"LASt Starting: {index} Ending: {endIndex}");
+
+for (var m = endIndex; m < track.Captions.Count; m++)
+{
+    var caption = track.Captions[m];
+    //caption = track.Captions[k];
+        //await file.WriteLineAsync($"TIMESTAMP: {caption.Offset}");
+        await file.WriteLineAsync($"{caption.Text}");
+}
 
 
 // how to get captions for 5 min chunks
