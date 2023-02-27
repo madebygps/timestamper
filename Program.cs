7b03039b6fd1dfe9f56db18c9a6a51f416dad93b
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-02-27 17:40:01 -0500

    Refactored to cli tool

--- Program.cs
+++ Program.cs
@@ -1,114 +1,119 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-// https://www.nuget.org/packages/YoutubeExplode
-
-using OpenAI.GPT3.Managers;
+﻿using OpenAI.GPT3.Managers;
 using OpenAI.GPT3.ObjectModels;
 using OpenAI.GPT3.ObjectModels.RequestModels;
 using YoutubeExplode;
 
-string youtubeUrl = "https://www.youtube.com/watch?v=LgWRbXw9dRU";
+using System.Reflection;
 
-var youtube = new YoutubeClient();
+namespace TeleprompterConsole;
 
-var openAiService = new OpenAIService(new OpenAI.GPT3.OpenAiOptions()
+internal class Program
 {
-    ApiKey = ""
-});
-
-// You can specify either video ID or URL
-var video = await youtube.Videos.GetAsync(youtubeUrl);
-
-//var title = video.Title; // "Collections - Blender 2.80 Fundamentals"
-//var author = video.Author.ChannelTitle; // "Blender"
-var duration = video.Duration; // 00:07:20
-
-using StreamWriter file = new("WriteLines.txt", append: true);
-
-var trackManifest = await youtube.Videos.ClosedCaptions.GetManifestAsync(
-    youtubeUrl
-);
-
-// Find closed caption track in English
-var trackInfo = trackManifest.GetByLanguage("en");
-
-var track = await youtube.Videos.ClosedCaptions.GetAsync(trackInfo);
-
-Console.WriteLine($"The amount of captions is {track.Captions.Count}");
-
-int slices = Convert.ToInt32(Math.Floor(duration.Value.TotalMinutes / 5)); // 11 because int
-int captionsPerSlice = track.Captions.Count / slices;
-
-Console.WriteLine($"The amount of slices is {slices}");
-Console.WriteLine($"The amount of captionSlices per slice is {captionsPerSlice}");
-
-int startIndex = 0;
-int endIndex = track.Captions.Count / slices;
-
-for (int l = 0; l < slices; l++)
-{
-    var caption = track.Captions[startIndex];
-    await file.WriteLineAsync($"TIMESTAMP: {caption.Offset}");
-    Console.WriteLine($"Starting: {startIndex} Ending: {endIndex}");
-    string words = "";
-
-    for (int k = startIndex; k < endIndex; k++)
+    public static async Task Main(string[] args)
     {
-
-        caption = track.Captions[k];
-        if (!string.IsNullOrWhiteSpace(caption.Text))
+        if (args.Length == 0)
         {
-            words += $"{caption.Text} ";
-            //await file.WriteLineAsync($"{caption.Text}");
+            var versionString = Assembly.GetEntryAssembly()?
+                                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                                    .InformationalVersion
+                                    .ToString();
+            Console.WriteLine($"timestamper v{versionString}");
+            Console.WriteLine("-------------");
+            Console.WriteLine("\nUsage:");
+            Console.WriteLine("  timestamper <videoUrl>");
+            return;
         }
-        // TODO: Make Open API calls for every time stamp chunk and save completion to a file. 
+        await GenerateCaptions(string.Join(' ', args));
     }
-    var completionResult = await openAiService.Completions.CreateCompletion(new CompletionCreateRequest()
-    {
-        Prompt = $"(Summarize the following in 10 words: {words} Summary:",
-        Model = Models.TextDavinciV3,
-        Temperature = (float?)0.67,
-        TopP = 1,
-        MaxTokens = 256,
-        FrequencyPenalty = 0,
-        PresencePenalty = 0
 
-    });
+    public static async Task GenerateCaptions(string message)
+    {
+        Console.WriteLine("youtubeUrl: " + message);
+        string youtubeUrl = message;
+        var youtube = new YoutubeClient();
 
-    if (completionResult.Successful)
-    {
-        Console.WriteLine(completionResult.Choices.FirstOrDefault());
-        await file.WriteLineAsync($"{completionResult.Choices.FirstOrDefault()}");
-    }
-    else
-    {
-        if (completionResult.Error == null)
+        var openAiService = new OpenAIService(new OpenAI.GPT3.OpenAiOptions()
         {
-            throw new Exception("Unknown Error");
-        }
-        Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
-    }
+            ApiKey = "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"
+        });
 
-    if (endIndex + captionsPerSlice < track.Captions.Count)
-    {
-        startIndex = startIndex + captionsPerSlice;
-        endIndex = endIndex + captionsPerSlice;
+        var trackManifest = await youtube.Videos.ClosedCaptions.GetManifestAsync(
+            youtubeUrl
+        );
+
+        // Find closed caption track in English
+        var trackInfo = trackManifest.GetByLanguage("en");
+
+        var track = await youtube.Videos.ClosedCaptions.GetAsync(trackInfo);
+
+        int slices = 20;
+        int captionsPerSlice = track.Captions.Count / slices;
+
+        int startIndex = 0;
+        int endIndex = captionsPerSlice;
+        string words = "";
+
+        for (int l = 0; l < slices; l++)
+        {
+            var caption = track.Captions[startIndex];
+
+            Console.Write(caption.Offset.ToString().Split('.')[0] + ": ");
+            words = "";
+
+            for (int k = startIndex; k < endIndex; k++)
+            {
+
+                caption = track.Captions[k];
+                if (!string.IsNullOrWhiteSpace(caption.Text))
+                {
+                    words += $"{caption.Text}";
+                }
+
+            }
+            var completionResult = await openAiService.Completions.CreateCompletion(new CompletionCreateRequest()
+            {
+                Prompt = $"(Summarize the following in 10 words: {words} Summary:",
+                Model = Models.TextDavinciV3,
+                Temperature = (float?)0.67,
+                TopP = 1,
+                MaxTokens = 256,
+                FrequencyPenalty = 0,
+                PresencePenalty = 0
+
+            });
+
+            if (completionResult.Successful)
+            {
+
+                string summary = completionResult.Choices.FirstOrDefault().ToString().Remove(0, 25);
+                int index = summary.IndexOf("Index =");
+                if (index >= 0)
+                {
+                    summary = summary.Substring(0, index);
+
+                }
+                Console.WriteLine(summary);
+            }
+            else
+            {
+                if (completionResult.Error == null)
+                {
+                    throw new Exception("Unknown Error");
+                }
+                Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+            }
+
+            if (endIndex + captionsPerSlice < track.Captions.Count)
+            {
+                startIndex = startIndex + captionsPerSlice;
+                endIndex = endIndex + captionsPerSlice;
+            }
+            else
+            {
+                startIndex = endIndex;
+            }
+        }
     }
 }
 
-Console.WriteLine($"Last Starting: {startIndex} Ending: {endIndex}");
-/*
-for (var m = endIndex; m < track.Captions.Count; m++)
-{
-    var caption = track.Captions[m];
-    // Check if the last caption is not empty
-    if (!string.IsNullOrWhiteSpace(caption.Text))
-    {
-        await file.WriteLineAsync($"{caption.Text}");
-    }
-
-}*/
-
-// TODO: Make Open API calls for every time stamp chunk and save completion to a file. 
-
-
+// You can specify either video ID or URL
